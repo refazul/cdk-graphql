d71f5605662f1226a27a5b8daa238379ad095f45
refazul <refazul.refat@yahoo.com>
2022-03-18 11:52:39 +0600

    aws-appsync api-key

--- lib/cdk-graphql-stack.ts
+++ lib/cdk-graphql-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as appsync from '@aws-cdk/aws-appsync';
+
 import { MyBackend } from './mybackend';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -21,20 +23,34 @@ export class CdkGraphqlStack extends cdk.Stack {
 			handler: myBackend.handler
 		});
 
-        /*
+		const myGraphqlApi = new appsync.GraphqlApi(this, "MyGraphqlApi", {
+			name: "my-book-api",
+			schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+			authorizationConfig: {
+				defaultAuthorization: {
+					authorizationType: appsync.AuthorizationType.API_KEY,
+					apiKeyConfig: {
+						name: "My very own API key",
+						expires: cdk.Expiration.after(cdk.Duration.days(365))
+					}
+				}
+			}
+		})
+
+		/*
 		const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
-            publicReadAccess: true,
-            websiteIndexDocument: "index.html"
-        });
+			publicReadAccess: true,
+			websiteIndexDocument: "index.html"
+		});
 
-        new s3Deployment.BucketDeployment(this, "DeployWebsite", {
-            destinationBucket: websiteBucket,
-            sources: [s3Deployment.Source.asset("frontend/build")]
-        });
+		new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+			destinationBucket: websiteBucket,
+			sources: [s3Deployment.Source.asset("frontend/build")]
+		});
 
-        new cdk.CfnOutput(this, "WebsiteAddress", {
-            value: websiteBucket.bucketWebsiteUrl
-        });
-        */
+		new cdk.CfnOutput(this, "WebsiteAddress", {
+			value: websiteBucket.bucketWebsiteUrl
+		});
+		*/
 	}
 }
